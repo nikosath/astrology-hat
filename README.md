c3011ebcba63b365efe27d66770f97fce6a83ef8
Nikos Athanasakis <emidekol@gmail.com>
2017-02-24 00:52:21 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Astrology Hat
+
+A Wordpress theme, created by myself, for a future small business web site.
+
+Astrology Hat is based on the [Start Bootstrap](http://startbootstrap.com/) template created by [David Miller](http://davidmiller.io/).
---
+++ footer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The footer for our theme.
+ *
+ * Displays all of the footer section following the page content and sidebar
+ *
+ * @package AstrologyHat
+ */
+?></div>
+<!-- /.row -->
+
+<hr>
+
+<!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2010</p>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <!-- <script src="js/jquery.js"></script> -->
+
+    <!-- Bootstrap Core JavaScript -->
+    <!-- <script src="js/bootstrap.min.js"></script> -->
+    <?php wp_footer(); ?>
+</body>
+
+</html>
